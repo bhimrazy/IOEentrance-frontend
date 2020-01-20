1a109479447930f3f0564cac7d98b8f6ba6ff35d
bhimrazy <bhimrajyadav977@gmail.com>
2020-01-20 16:09:50 +0545

    Frontend part home completed

--- js/main.js
+++ js/main.js
@@ -1,51 +1,36 @@
-let nCount = selector => {
-    $(selector).each(function () {
-        $(this)
-            .animate({
-                Counter: $(this).text()
-            }, {
-                // A string or number determining how long the animation will run.
-                duration: 4000,
-                // A string indicating which easing function to use for the transition.
-                easing: "swing",
-                /**
-                 * A function to be called for each animated property of each animated element. 
-                 * This function provides an opportunity to
-                 *  modify the Tween object to change the value of the property before it is set.
-                 */
-                step: function (value) {
-                    $(this).text(Math.ceil(value));
-                }
-            });
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function () {
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
-};
-
-let a = 0;
-$(window).scroll(function () {
-    // The .offset() method allows us to retrieve the current position of an element  relative to the document
-    let oTop = $(".numbers").offset().top - window.innerHeight;
-    if (a == 0 && $(window).scrollTop() >= oTop) {
-        a++;
-        nCount(".rect > h1");
-    }
-});
 
 
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
 
-/**
- *
- *  sticky navigation
- *
- */
-
-let navbar = $(".navbar");
-
-$(window).scroll(function () {
-    // get the complete hight of window
-    let oTop = $(".section-1").offset().top - window.innerHeight;
-    if ($(window).scrollTop() > oTop) {
-        navbar.addClass("sticky");
-    } else {
-        navbar.removeClass("sticky");
-    }
 });
